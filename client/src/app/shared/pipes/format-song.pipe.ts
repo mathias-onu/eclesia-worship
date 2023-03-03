1d35ca5ebf8d1007b4ef883aafb526c4bb5bcf68
Denis-Onu <deni_onu@yahoo.com>
2023-03-03 12:01:16 +0200

    formad

--- client/src/app/shared/pipes/format-song.pipe.ts
+++ client/src/app/shared/pipes/format-song.pipe.ts
@@ -1,5 +1,4 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { ChordProParser, FormatterSettings, TextFormatter } from 'chordproject-parser';
 import { IFormattedSong, ISong } from '../models/song.model';
 import { chordProParser } from '../utils/chordProParser';
 import { textTypeAccessor } from '../utils/textType.enum';
@@ -11,6 +10,30 @@ export class FormatSongPipe implements PipeTransform {
 
   transform(value: ISong): IFormattedSong {
     const formattedSong = chordProParser(value, textTypeAccessor.SONG)
+
+    console.log(formattedSong)
+
+    const songContent: any = {
+      title: value.title,
+      verses: [
+        { verse: 0, lines: [] }
+      ]
+    }
+    let verseIndex = 0
+    for (let i = 0; i < formattedSong.length; i++) {
+      const line = formattedSong[i]
+
+      if (line === "" && i !== formattedSong.length - 1) {
+        verseIndex++
+        songContent.verses.push({ verse: verseIndex, lines: [] })
+      } else if (verseIndex > 0 && !line.includes('#')) {
+        songContent.verses[verseIndex].lines.push(line)
+      }
+    }
+
+    songContent.verses.shift()
+
+    console.log(songContent)
 
     const song = formattedSong.join(' ').split("  ")
     let songObj = {
