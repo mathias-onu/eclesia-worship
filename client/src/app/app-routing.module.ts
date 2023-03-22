75ca5f97dc2c962aa7245acd2857668787d169fd
mathias-onu <mathias.onu@outlook.com>
2023-03-22 19:17:15 +0200

    fixed reauthorization issue when the token expires

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthorizeComponent } from './core/components/authorize/authorize.component';
 import { AuthGuard } from './core/guards/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./features/controller-view/controller-view.module').then(m => m.ControllerViewModule), canActivate: [AuthGuard], data: { authGuardRedirect: '/home' } },
   { path: 'live', loadChildren: () => import('./features/presentation-view/presentation-view.module').then(m => m.PresentationViewModule), canActivate: [AuthGuard], data: { authGuardRedirect: '/live' } },
+  { path: 'authorize', component: AuthorizeComponent, pathMatch: 'full' }
 ];
 
 @NgModule({
