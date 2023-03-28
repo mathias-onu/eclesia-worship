9158f9ba3f9efe343b0b53be7a836e6f41264344
mathias-onu <mathias.onu@outlook.com>
2023-03-28 16:28:22 +0300

    added more waits to the e2e tests for debugging

--- client/cypress/e2e/presentation-flows.cy.ts
+++ client/cypress/e2e/presentation-flows.cy.ts
@@ -6,7 +6,8 @@ describe("Testing basic presentation flows.", () => {
 
   it("should add a song to the playlist, add the song the the pre-presentation section, and the present the song", () => {
     // ACT
-    cy.get('[data-testid="searchSongsInput"]').type('Biruitor', { force: true })
+    cy.wait(3000)
+    cy.get('[data-testid="searchSongsInputs"]').type('Biruitor', { force: true })
     cy.get('[data-testid="searchSongsBtn"]').click()
     cy.get('span').contains('Biruitor', { timeout: 5000 }).siblings('div').find('[data-testid="addSongToPlaylistBtn"]').eq(0).click()
 
@@ -18,8 +19,9 @@ describe("Testing basic presentation flows.", () => {
 
   it("should open the playlists dialog, select the latest playlist, and present the first song from the playlist", () => {
     // ACT   
+    cy.wait(3000)
     cy.get('[data-testid="openPlaylistSearchBtn"]').click()
-    cy.get('[data-testid="expandPlaylistBtn"]').eq(0).click()
+    cy.get('[data-testid="expandPlaylistBtn"]', { timeout: 5000 }).eq(0).click()
     cy.get('[data-testid="selectPlaylistBtn"]').eq(0).click()
     cy.get('[data-testid="addSongToPresentationBtn"]').eq(0).click()
 
