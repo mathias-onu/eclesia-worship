f2cad3a46c3f88bfe9a66ec2929ad5fcbab7145a
mathias-onu <mathias.onu@outlook.com>
2023-03-29 12:45:09 +0300

    added new partial sync endpoints for db seeding while running e2e tests

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,9 +5,11 @@ const router = express.Router()
 import {
   refreshToken,
   syncSongs,
+  syncSongsPartial,
   getSong,
   getSongs,
   syncPlaylists,
+  syncPlaylistsPartial,
   getPlaylist,
   getPlaylists,
   getBible
@@ -16,10 +18,12 @@ import protect from '../middleware/authMiddleware.js'
 
 router.post('/refresh-token', refreshToken)
 router.post('/sync/songs', protect, syncSongs)
+router.post('/sync-partial/songs', protect, syncSongsPartial)
 router.get('/songs', protect, getSongs)
 router.get('/songs/:id', protect, getSong)
 
 router.post('/sync/playlists', protect, syncPlaylists)
+router.post('/sync-partial/playlists', protect, syncPlaylistsPartial)
 router.get('/playlists', protect, getPlaylists)
 router.get('/playlists/:id', protect, getPlaylist)
 
