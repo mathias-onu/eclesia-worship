4a6b045e4486c062fb8789bf55be7ee271845b43
mathias-onu <mathias.onu@outlook.com>
2023-04-05 11:27:51 +0300

    added 404 page not found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthorizeComponent } from './core/components/authorize/authorize.component';
 import { AuthGuard } from './core/guards/auth.guard';
+import { PageNotFoundComponent } from './shared/components/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./features/controller-view/controller-view.module').then(m => m.ControllerViewModule), canActivate: [AuthGuard], data: { authGuardRedirect: '/home' } },
   { path: 'live', loadChildren: () => import('./features/presentation-view/presentation-view.module').then(m => m.PresentationViewModule), canActivate: [AuthGuard], data: { authGuardRedirect: '/live' } },
-  { path: 'authorize', component: AuthorizeComponent, pathMatch: 'full' }
+  { path: 'authorize', component: AuthorizeComponent, pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
