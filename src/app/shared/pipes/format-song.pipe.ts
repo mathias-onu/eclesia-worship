25d4b51c12ec3f3828f30b0def116a4379fd636a
Denis Onu <deni_onu@yahoo.com>
2023-11-03 13:59:42 +0200

    comments update in song pipe

--- src/app/shared/pipes/format-song.pipe.ts
+++ src/app/shared/pipes/format-song.pipe.ts
@@ -22,7 +22,7 @@ export class FormatSongPipe implements PipeTransform {
 
     console.log(formattedSong)
 
-    // the loop will detect any 
+    // the loop will detect any verses containing more than 2 lines and will split them
     let verseCount: number = 0
     for(let i = 0; i < formattedSong.length; i++) {
       if(formattedSong[i] !== "" && formattedSong[i] !== '**********') {
@@ -58,35 +58,16 @@ export class FormatSongPipe implements PipeTransform {
       }
     }
 
-    // this is to remove the title of the song
+    // removes the title of the song
     songContent.verses.shift()
 
-    // this loop will 
+    // this loop will detect if there's an author verse and will remove it
     for(let i = 0; i < songContent.verses[0].lines.length; i++) {
       const verse = songContent.verses[0].lines[i]
       if(verse === "**********") {
         songContent.verses.shift()
-        console.log('dude')
       }
     }
-
-    // for (let i = 0; i < songContent.verses.length; i++) {
-    //   const slide = songContent.verses[i]
-    //   if (slide.lines.length === 4) {
-    //     songContent.verses.splice(i, 1)
-    //     const nextSlide = { verseIndex: i + 1, lines: [slide.lines[2], slide.lines[3]] }
-    //     slide.lines.pop()
-    //     slide.lines.pop()
-    //     songContent.verses.splice(i, 0, slide)
-    //     songContent.verses.splice(i + 1, 0, nextSlide)
-    //   }
-    // }
-
-    // for (let i = 0; i < songContent.verses.length; i++) {
-    //   if (songContent.verses[i].lines.length === 0) {
-    //     songContent.verses.splice(i, 1)
-    //   }
-    // }
 
     return songContent
   }
