4d72cce442b446f89b4c76250ecfcdfc288aa503
Denis Onu <deni_onu@yahoo.com>
2023-11-26 19:53:45 +0200

    removed mongodb collation for atlas compatibility

--- server/controllers/songs.ts
+++ server/controllers/songs.ts
@@ -85,7 +85,6 @@ export const getSongs = asyncHandler(async (req: Request, res: Response) => {
     const songs = await Song.find({
         title: { $regex: diacriticSensitiveRegex(search?.toString()) || "", $options: "i" },
     })
-        .collation({ locale: "ro", strength: 1 })
         .limit(Number(limit)).sort({ title: 1 });
     res.json(songs);
 });
