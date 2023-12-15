23f6c4fcd11e02500ba316d0724181a2facadc80
Denis Onu <deni_onu@yahoo.com>
2023-12-15 13:41:35 +0200

    added local Bible

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -4,10 +4,12 @@ const router = express.Router()
 
 import {
   refreshToken,
-  getBible
+  getBible,
+  importBible,
 } from '../controllers/index.js'
 
 router.post('/refresh-token', refreshToken)
+router.post('/import-bible', importBible)
 router.get('/bible', getBible)
 
 export default router
