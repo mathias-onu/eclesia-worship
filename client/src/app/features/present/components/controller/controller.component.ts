e73a7bbb010e2d375a44c783730da7b52c448ca8
mathias-onu <mathias.onu@outlook.com>
2024-02-24 01:37:47 +0200

    added persistence of splitter values feature

--- client/src/app/features/present/components/controller/controller.component.ts
+++ client/src/app/features/present/components/controller/controller.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LocalStorageService } from 'ngx-webstorage';
+import { SplitterResizeEndEvent } from 'primeng/splitter';
 
 @Component({
   selector: 'app-controller',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './controller.component.scss'
 })
 export class ControllerComponent implements OnInit {
+  panel1Size: number = 30
+  panel2Size: number = 70
 
-  constructor() {}
+  constructor(private localStorageService: LocalStorageService) {}
 
   ngOnInit(): void {
-    
+    this.panel1Size = this.localStorageService.retrieve('panel1Size')
+    this.panel2Size = this.localStorageService.retrieve('panel2Size')
+  }
+
+  splitterChange(event: SplitterResizeEndEvent) {
+    this.localStorageService.store('panel1Size', event.sizes[0])
+    this.localStorageService.store('panel2Size', event.sizes[1])
   }
 }
